7e17a38dcca61a401c3f37afca45aa91075bdaaf
KnoyanMitsu <yanappuowo@yandex.com>
2025-02-24 13:37:14 +0000

    Praktikum-5

--- resources/views/kategori.blade.php
+++ resources/views/kategori.blade.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Data Level Pengguna</title>
+        <title>Data Kategori Barang</title>
     </head>
     <body>
-        <h1>Data Level Pengguna</h1>
+        <h1>Data Kategori Barang</h1>
         <table border="1" cellpadding="" cellspacing="0">
             <tr>
                 <th>ID</th>
-                <th>Kode Level</th>
-                <th>Nama Level</th>
+                <th>Kode Kategori</th>
+                <th>Nama Kategori</th>
             </tr>
             @foreach ($data as $d)
             <tr>
-                <td>{{ $d->level_id }}</td>
-                <td>{{ $d->level_kode }}</td>
-                <td>{{ $d->level_nama }}</td>
+                <td>{{ $d->kategori_id }}</td>
+                <td>{{ $d->kategori_kode }}</td>
+                <td>{{ $d->kategori_nama }}</td>
             </tr>
             @endforeach
         </table>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
@@ -23,13 +23,14 @@ Route::get('/', function () {
 
 Route::get('/level',[LevelController::class, 'index']);
 
+Route::get('/kategori',[KategoriController::class, 'index']);
 
-Route::prefix('category')->group(function (){
-    Route::get('/food-beverage', [CategoryController::class,'beverage'])->name("beverage");
-    Route::get('/beauty-health', [CategoryController::class,'beauty'])->name("beauty");
-    Route::get('/home-care', [CategoryController::class,'homecare'])->name("homecare");
-    Route::get('/baby-kid', [CategoryController::class,'babykid'])->name("babykid");
-});
+// Route::prefix('category')->group(function (){
+    // Route::get('/food-beverage', [CategoryController::class,'beverage'])->name("beverage");
+    // Route::get('/beauty-health', [CategoryController::class,'beauty'])->name("beauty");
+    // Route::get('/home-care', [CategoryController::class,'homecare'])->name("homecare");
+    // Route::get('/baby-kid', [CategoryController::class,'babykid'])->name("babykid");
+// });
 
 Route::get('/user/{id}/name/{name}',[UserController::class,'user']);
 
