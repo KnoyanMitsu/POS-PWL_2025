b9068ea85174c855ea0cf141744359b1f1a2e151
KnoyanMitsu <yanappuowo@yandex.com>
2025-03-03 03:10:51 +0000

    Praktikum-2.1

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,22 +15,26 @@ class UserController extends Controller
     //
 
     public function index(){
-        $data = [
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345'),
-            'level_id' => 2
-        ];
-        UserModel::create($data);
-        // UserModel::insert($data);
+        $user = UserModel::findOr(20,['username','nama'],function(){
+            abort(404);
+        });
 
+        // $data = [
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345'),
+        //     'level_id' => 2
+        // ];
+        // UserModel::create($data);
+        // UserModel::insert($data);
+        //
         //  $data = [
         //     'nama' => 'Pelanggan Pertama',
         // ];
         // UserModel::where('username', 'customer-1')->update($data);
-
+        //
         // UserModel::find(7)->delete();
-        $user = UserModel::all();
+        // $user = UserModel::all();
         return view('user',['data' => $user]);
     }
 }
